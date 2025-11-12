a842927b6bbd7eedbb706c45217f78c2e953d1ae
Pavlo Konovchenko <c00312835@setu.ie>
2025-11-12 14:15:15 +0000

    refactor: minesweeper board into its own class

--- 16_SFML_Games/minesweeper.cpp
+++ 16_SFML_Games/minesweeper.cpp
@@ -2,6 +2,53 @@
 #include <time.h>
 using namespace sf;
 
+class Board
+{
+public:
+    Board()
+    {
+        for (int i = 1; i <= 10; i++)
+            for (int j = 1; j <= 10; j++)
+            {
+                shownGrid[i][j] = 10;
+                if (rand() % 5 == 0)  grid[i][j] = 9;
+                else grid[i][j] = 0;
+            }
+
+        for (int i = 1; i <= 10; i++)
+            for (int j = 1; j <= 10; j++)
+            {
+                int n = 0;
+                if (grid[i][j] == 9) continue;
+                if (grid[i + 1][j] == 9) n++;
+                if (grid[i][j + 1] == 9) n++;
+                if (grid[i - 1][j] == 9) n++;
+                if (grid[i][j - 1] == 9) n++;
+                if (grid[i + 1][j + 1] == 9) n++;
+                if (grid[i - 1][j - 1] == 9) n++;
+                if (grid[i - 1][j + 1] == 9) n++;
+                if (grid[i + 1][j - 1] == 9) n++;
+                grid[i][j] = n;
+            }
+    };
+
+    void revealGrid(int t_x, int t_y)
+    {
+        shownGrid[t_x][t_y] = grid[t_x][t_y];
+    }
+    void setFlag(int t_x, int t_y)
+    {
+        shownGrid[t_x][t_y] = 11;
+    }
+    int getTileValue(int t_x, int t_y)
+    {
+        return shownGrid[t_x][t_y];
+    }
+private:
+    int grid[12][12];
+    int shownGrid[12][12];
+};
+
 int minesweeper()
 {
     srand(time(0));
@@ -9,40 +56,20 @@ int minesweeper()
     RenderWindow app(VideoMode(400, 400), "Minesweeper!");
 
     int w=32;
-    int grid[12][12];
-    int sgrid[12][12]; //for showing
+    Board board;
 
     Texture t;
     t.loadFromFile("images/minesweeper/tiles.jpg");
     Sprite s(t);
-
-    for (int i=1;i<=10;i++)
-     for (int j=1;j<=10;j++)
-      {
-        sgrid[i][j]=10;
-        if (rand()%5==0)  grid[i][j]=9;
-        else grid[i][j]=0;
-      }
-
-    for (int i=1;i<=10;i++)
-     for (int j=1;j<=10;j++)
-      {
-        int n=0;
-        if (grid[i][j]==9) continue;
-        if (grid[i+1][j]==9) n++;
-        if (grid[i][j+1]==9) n++;
-        if (grid[i-1][j]==9) n++;
-        if (grid[i][j-1]==9) n++;
-        if (grid[i+1][j+1]==9) n++;
-        if (grid[i-1][j-1]==9) n++;
-        if (grid[i-1][j+1]==9) n++;
-        if (grid[i+1][j-1]==9) n++;
-        grid[i][j]=n;
-      }
-
+    
+    //
+    // Game loop
+    //
     while (app.isOpen())
     {
         Vector2i pos = Mouse::getPosition(app);
+        // Divide by cingular cell sprite size
+        // to get which cell we are currently clicking
         int x = pos.x/w;
         int y = pos.y/w;
 
@@ -52,18 +79,24 @@ int minesweeper()
             if (e.type == Event::Closed)
                 app.close();
 
+            // if mouse was pressed open the cell that was pressed
+            // or mark it as a flag
             if (e.type == Event::MouseButtonPressed)
-              if (e.key.code == Mouse::Left) sgrid[x][y]=grid[x][y];
-              else if (e.key.code == Mouse::Right) sgrid[x][y]=11;
+              if (e.key.code == Mouse::Left) board.revealGrid(x, y);
+              else if (e.key.code == Mouse::Right) board.setFlag(x, y);
         }
 
+
+        //
+        // Draw
+        //
         app.clear(Color::White);
 
         for (int i=1;i<=10;i++)
          for (int j=1;j<=10;j++)
           {
-           if (sgrid[x][y]==9) sgrid[i][j]=grid[i][j];
-           s.setTextureRect(IntRect(sgrid[i][j]*w,0,w,w));
+           if (board.getTileValue(x, y) == 9) board.revealGrid(i, j);;
+           s.setTextureRect(IntRect(board.getTileValue(i, j)*w,0,w,w));
            s.setPosition(i*w, j*w);
            app.draw(s);
           }
